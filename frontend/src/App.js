7090ce84add219fa4ccbd29441f24648b03cc360
Apurba Debnath <62664361+apurba2002@users.noreply.github.com>
2023-07-21 21:58:16 +0600

    jhjhjjk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,17 +8,19 @@ import Odyssey from './Odyssey';
 import Footer from './footer';
 import Contact from './contact';
 import About from './about';
+import Prometheus_details from './prometheusdetail';
 
 function App() {
   return (
     <>
       <NAvbar />
-      <section id='home'>
+      <section id='home' style={{display:"none"}}>
         <Main />
         <Prometheus />
         <Hyperion />
         <Odyssey />
       </section>
+      <Prometheus_details />
       <About />
       <Contact />
       <Footer />
---
+++ frontend/src/prometheusdetail.js
@@ -0,0 +1,298 @@
+import './App.css';
+function Prometheus_details() {
+    return (
+        <>
+            <div className="prod">
+                <h1>
+                    The ERA Prometheus
+                </h1>
+                <iframe src="https://www.youtube.com/embed/0ubBkT1z5HA?clip=UgkxYl4v67GnL0rE0Irx6X4GJsi2ICphT4sv&amp;clipt=EJCvAhjwgwY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <p>
+                    In 2018, we set out to create the world's first attainable Tourbillon timepiece. Watch the video above in full to see how this simply changes EVERYTHING
+                </p>
+                <div className="img-row-1">
+                    <img src="pd1.jpg" alt=""></img>
+                    <img src="pd2.jpg" alt=""></img>
+
+                </div>
+                <div className="img-row-2">
+                    <img src="pd3.jpg" alt=""></img>
+                    <img src="pd5.jpg" alt=""></img>
+
+                </div>
+                <div className="img-row-3">
+                    <img src="p1.jpg" alt=""></img>
+                    <img src="pd7.jpg" alt=""></img>
+
+                </div>
+                <button>
+                    Buy Now
+                </button>
+                <div className="details">
+                    <div className="left">
+                        <h1>
+                            MODEL CASE
+                        </h1>
+
+                        <hr></hr>
+                        <p>
+                            MODEL VERSIONS
+                        </p>
+                        <p>
+                            Prometheus Standard Edition in:
+                        </p>
+                        <p>
+                            Hand-Polished 316L Grade
+                        </p>
+                        <p>
+                            Stainless Steel Case
+                        </p>
+                        <p>
+                            OR
+                        </p>
+                        <p>
+                            Platinum-Plated Case
+                        </p>
+                        <p>
+                            OR
+                        </p>
+                        <p>
+                            Brushed DLC (Diamond-Like Carbon) Coated Case
+                        </p>
+                        <p>
+                            Prometheus Titan Edition in:
+                        </p>
+                        <p>
+                            Platinum
+                        </p>
+                        <p>
+                            OR
+                        </p>
+                        <p>
+                            Brushed DLC (Diamond-Like Carbon) Coated Case
+                        </p>
+                        <p>
+                            DIAMETER
+                        </p>
+                        <p>
+                            44 mm
+                        </p>
+                        <p>
+                            THICKNESS
+                        </p>
+                        <p>
+                            12.72 mm
+                        </p>
+                        <p>
+                            OR
+                        </p>
+                        <p>
+                            14.72 mm
+                        </p>
+                        <p>
+                            WEIGHT
+                        </p>
+                        <p>
+                            125 grams
+                        </p>
+                        <p>
+                            WINDING CROWN
+                        </p>
+                        <p>
+                            Elegant Spiral / Brushed Hex
+                        </p>
+                        <p>
+                            CRYSTAL
+                        </p>
+                        <p>
+                            Ultra Scratch-resistant Sapphire Glass With Anti-Reflective Coating
+                        </p>
+                        <p>
+                            WATER RESISTANCE
+                        </p>
+                        <p>
+                            5 ATM / 50 meters
+                        </p>
+                        <p>
+                            TITAN ADDITIONAL FEATURE
+                        </p>
+                        <p>
+                            78+ diamonds weighing 2.3+ carats fused into the entire bezel of the watch, all Bright cut round GH colored diamonds with a clarity of VS1
+                        </p>
+                        <p>
+                            TITAN ADDITIONAL FEATURE
+                        </p>
+                        <p>
+                            Ability to customize/engrave the back of the watch with a message
+                        </p>
+
+                    </div>
+                    <div className="mid">
+                        <h1>
+                            MOVEMENT
+                        </h1>
+
+                        <hr></hr>
+                        <p>
+                            MOVEMENT
+                        </p>
+                        <p>
+                            Single-Axis Tourbillon Movement, Automatic Movement With Engraved Rotor, GMT Hour Hand, Sun/Moon Time of Day Indicator
+                        </p>
+                        <p>
+                            CALIBRE
+                        </p>
+                        <p>
+                            ERA-004 Calibre
+                        </p>
+                        <p>
+                            PRECISION
+                        </p>
+                        <p>
+                            Regulated To +/- 10 sec or better
+                        </p>
+                        <p>
+                            FUNCTIONS
+                        </p>
+                        <p>
+                            Centre hour and minute hands, Second time zone with independent setting of the hour hand, Time of Day Indicator, Seconds hand functionality through revolution of Tourbillon complication
+                        </p>
+                        <p>
+                            FREQUENCY
+                        </p>
+                        <p>
+                            28,800 vph
+                        </p>
+                        <p>
+                            JEWELS
+                        </p>
+                        <p>
+                            20
+                        </p>
+                        <p>
+                            POWER RESERVE
+                        </p>
+                        <p>
+                            ~32 hours
+                        </p>
+
+
+
+                    </div>
+                    <div className="right">
+                        <h1>
+                            STRAP
+                        </h1>
+
+                        <hr></hr>
+                        <p>
+                            STRAP
+                        </p>
+                        <p>
+                            Hornback Style Leather Strap With Quick Release Bars (manual)
+                        </p>
+                        <p>
+                            OR
+                        </p>
+                        <p>
+                            Alligator Style Leather Strap With Quick Release Bars (automatic)
+                        </p>
+                        <p>
+                            STRAP DIMENSIONS
+                        </p>
+                        <p>
+                            24mm
+                        </p>
+                        <p>
+                            STRAP MATERIAL
+                        </p>
+                        <p>
+                            100% Genuine Leather
+                        </p>
+                        <p>
+                            STITCHING
+                        </p>
+                        <p>
+                            Premium Double-Stitched
+                        </p>
+                        <p>
+                            Threading
+                        </p>
+                        <p>
+                            CLASP
+                        </p>
+                        <p>
+                            Folding ERA Butterfly Clasp
+                        </p>
+                        <p>
+                            OR
+                        </p>
+                        <p>
+                            Custom In-House ERA Butterfly
+                        </p>
+                        <p>
+                            Clasp
+                        </p>
+
+                    </div>
+
+                </div>
+                <button>
+                    Buy Now
+                </button>
+                <div className="img-row-4">
+                    <img src="pd8.jpg" alt=""></img>
+                    <img src="pd9.jpg" alt=""></img>
+                    <img src="pd10.jpg" alt=""></img>
+
+                </div>
+                <div className="img-row-5">
+                    <img src="pd11.jpg" alt=""></img>
+                    <img src="pd12.jpg" alt=""></img>
+                    <img src="pd13.jpg" alt=""></img>
+
+                </div>
+                <button>
+                    Buy Now
+                </button>
+                <div className="warenty">
+                    <h1>
+                        Try an ERA Risk-FREE. Guaranteed.
+                        We'll ship you one to try
+                    </h1>
+                    <div className="grp">
+                        <div className="left">
+                            <button>60</button>
+                            <h1>
+                                60-Day Watch Trial
+                            </h1>
+                            <p>
+                                We know that you need to see the timepiece on your wrist before you know if it's the right match for you. So take your time -- we'll give you a full 60 days to test it out. If it's not for you, we'll even cover return shipping costs *
+
+                            </p>
+
+                        </div>
+                        <div className="right">
+                            <button>2yrs</button>
+                            <h1>
+                                2-Year Warranty
+                            </h1>
+                            <p>
+                                We know that mechanical timepieces require an appropriate support system in place. That's why we have an amazing international 2 year warranty service that includes FREE express global return shipping with 100% insurance.
+
+
+                            </p>
+                        </div>
+                    </div>
+                    <button>
+                        Buy Now
+                    </button>
+                    <img src="pdlast.jpg" alt="" className="last-img"></img>
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+export default Prometheus_details;
